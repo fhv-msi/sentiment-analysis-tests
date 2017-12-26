b76cf40a3cff7bf908c9dac90b0f1278d5049295
Unknown <michael_sieber@vol.at>
2017-12-26 10:43:09 +0100

    add python script for downloading the test video

---
+++ scripts/get_testingbot_video.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import json, sys, urllib, os;
+
+url = "https://" + os.environ['TESTINGBOT_CREDENTIALS'] + "@api.testingbot.com/v1/tests?count=10?skip_fields=logs,thumbs,steps"
+jsonurl = urllib.urlopen(url)
+response = json.loads(jsonurl.read())
+
+# get the video link of the latest test with the name "User interaction with history"
+for test in response['data']:
+    if test['name'] == 'User interaction with history' and test['state'] == 'COMPLETE':
+        print test['video']
+        # download the Video
+        urllib.urlretrieve (test['video'], "walk_through.mp4")
+        sys.exit(0)

21e5622d52eb175d1744e56b6d6400fed84a44f2
Unknown <michael_sieber@vol.at>
2017-12-29 19:59:45 +0100

    add debug output

--- scripts/get_testingbot_video.py
+++ scripts/get_testingbot_video.py
@@ -4,9 +4,13 @@ import json, sys, urllib, os;
 url = "https://" + os.environ['TESTINGBOT_CREDENTIALS'] + "@api.testingbot.com/v1/tests?count=10?skip_fields=logs,thumbs,steps"
 jsonurl = urllib.urlopen(url)
 response = json.loads(jsonurl.read())
+print response
+print "***************************"
 
 # get the video link of the latest test with the name "User interaction with history"
 for test in response['data']:
+    print test
+    print "############################"
     if test['name'] == 'User interaction with history' and test['state'] == 'COMPLETE':
         print test['video']
         # download the Video
